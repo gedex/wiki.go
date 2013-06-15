fa83e6f23ba5d9a8dc5629fe9b8378015779c39c
Akeda Bagus <akeda@x-team.com>
2013-06-16 05:25:23 +0700

    Init.

---
+++ README.md
@@ -0,0 +1,21 @@
+Wiki.go
+=======
+
+My first stab at writing web apps in Go by folllowing [this article](http://golang.org/doc/articles/wiki/).
+
+## Run it
+
+~~~
+go build wiki.go
+./wiki
+open http://localhost:8080
+~~~
+
+## TODO
+
+* Use DB instead of text files.
+* Add user login and register handler
+* Use session for logged-in user
+* Add history of a Page
+* Add diff of a Page revision
+* Allow Markdown syntax and/or safe HTML markups
---
+++ wiki.go
@@ -0,0 +1,93 @@
+package main
+
+import (
+	"html/template"
+	"io/ioutil"
+	"net/http"
+	"regexp"
+)
+
+var templates = template.Must(template.ParseFiles("tmpl/edit.html", "tmpl/view.html"))
+
+var titleValidator = regexp.MustCompile("^[a-zA-Z0-9_-]+$")
+
+const lenPath = len("/view/")
+
+type Page struct {
+	Title string
+	Body []byte
+}
+
+func (p *Page) save() error {
+	filename := "data/" + p.Title + ".txt"
+	return ioutil.WriteFile(filename, p.Body, 0600)
+}
+
+func loadPage(title string) (*Page, error) {
+	filename := "data/" + title + ".txt"
+	body, err := ioutil.ReadFile(filename)
+	if err != nil {
+		return nil, err
+	}
+	return &Page{Title: title, Body: body}, nil
+}
+
+func renderTemplate(w http.ResponseWriter, tmpl string, p *Page) {
+	err := templates.ExecuteTemplate(w, tmpl + ".html", p)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}
+
+func viewHandler(w http.ResponseWriter, r *http.Request, title string) {
+	p, err := loadPage(title)
+	if err != nil {
+		http.Redirect(w, r, "/edit/" + title, http.StatusFound)
+		return
+	}
+	renderTemplate(w, "view", p)
+}
+
+func editHandler(w http.ResponseWriter, r *http.Request, title string) {
+	p, err := loadPage(title)
+	if err != nil {
+		p = &Page{Title: title}
+	}
+	renderTemplate(w, "edit", p)
+}
+
+func saveHandler(w http.ResponseWriter, r *http.Request, title string) {
+	body := r.FormValue("body")
+	p := &Page{Title: title, Body: []byte(body)}
+	err := p.save()
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	http.Redirect(w, r, "/view/" + title, http.StatusFound)
+}
+
+func makeHandler(fn func (http.ResponseWriter, *http.Request, string)) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		title := r.URL.Path[lenPath:]
+		if !titleValidator.MatchString(title) {
+			http.NotFound(w, r)
+			return
+		}
+		fn(w, r, title)
+	}
+}
+
+func main() {
+	http.HandleFunc("/view/", makeHandler(viewHandler))
+	http.HandleFunc("/edit/", makeHandler(editHandler))
+	http.HandleFunc("/save/", makeHandler(saveHandler))
+	http.HandleFunc("/", func (w http.ResponseWriter, r *http.Request) {
+		if r.URL.Path != "/" {
+			http.NotFound(w, r)
+			return
+		}
+		http.Redirect(w, r, "/view/FrontPage", http.StatusFound)
+	})
+	http.ListenAndServe(":8080", nil)
+}
